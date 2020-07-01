dc2c3403568fe29b77a40f150c4a3952dece6c4c
Christoph <christoph.leichte@googlemail.com>
2020-07-02 00:38:45 +0200

    Add migrations in models.py

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, String, create_engine
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import json
 import os
 
@@ -16,7 +17,8 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
+    migrate = Migrate(app, db)
+    #db.create_all()
 
 
 '''
