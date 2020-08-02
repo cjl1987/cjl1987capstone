444e90ce0d829404963ad8817ad16195e2cfc6ed
Christoph <christoph.leichte@googlemail.com>
2020-08-02 15:22:32 +0200

    Finally project is initialized and runs

--- setup.sh
+++ setup.sh
@@ -4,11 +4,13 @@ echo 'Script gestartet'
 
 #export DATABASE_URL="postgresql://boss:test1234@localhost:5432/capstone"
 
+#export DATABASE_URL="postgresql://chris:geheimwort@localhost:5432/capstone"
 export DATABASE_URL="postgresql://chris:geheimwort@localhost:5432/capstone"
+
 
 #export DATABASE_URL="postgres://localhost:5432/capstone"
 
 
-export EXCITED="false"
+export EXCITED="true"
 
 
